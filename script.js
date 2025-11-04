4fbcf7791b4a8f3263586a000d839bd8a7b8e9d8
Daniel Chassagnez Goncalves <daniel.chassagn@gmail.com>
2025-11-04 16:07:30 +0100

    add search trips

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const searchButton = document.getElementById('searchButton');
-searchButton.addEventListener('click', () => {
-    const departure = document.getElementsById('inputdeparture').value;
+searchButton.addEventListener('click', async () => {
+    const departure = document.getElementById('inputdeparture').value;
     const arrival = document.getElementById('inputarrival').value
+    const date = document.getElementById('date').value;
+    console.log(typeof date);
+    try {
+        const query = `?departure=${encodeURIComponent(departure)}&arrival=${encodeURIComponent(arrival)}&date=${encodeURIComponent(date)}`;
+        const response = await fetch(`http://localhost:3000/trips/search${query}`);
+        const data = await response.json();
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
 })
