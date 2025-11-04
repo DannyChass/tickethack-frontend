a4dd7058692e6f99c91826946297198b6b913aa0
Daniel Chassagnez Goncalves <daniel.chassagn@gmail.com>
2025-11-04 17:04:27 +0100

    show trips

--- script.js
+++ script.js
@@ -3,12 +3,37 @@ searchButton.addEventListener('click', async () => {
     const departure = document.getElementById('inputdeparture').value;
     const arrival = document.getElementById('inputarrival').value
     const date = document.getElementById('date').value;
-    console.log(typeof date);
     try {
         const query = `?departure=${encodeURIComponent(departure)}&arrival=${encodeURIComponent(arrival)}&date=${encodeURIComponent(date)}`;
         const response = await fetch(`http://localhost:3000/trips/search${query}`);
         const data = await response.json();
         console.log(data);
+        const tripContainer = document.getElementById('tripsContainer');
+        tripContainer.innerHTML = '';
+        for (let i = 0; i < 3; i++) {
+            const tripModel = document.createElement('div');
+            tripModel.classList.add('tripsModel');
+
+            const pTrip = document.createElement('p');
+            pTrip.innerHTML = `${data[i].departure} > ${data[i].arrival}`;
+            tripModel.appendChild(pTrip);
+
+            const pHour = document.createElement('p');
+            const dateTrip = new Date(data[i].date);
+            pHour.textContent = dateTrip.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+            tripModel.appendChild(pHour);
+
+            const pPrice = document.createElement('p');
+            pPrice.innerHTML=`${data[i].price}€`;
+            tripModel.appendChild(pPrice);
+
+            bookButton = document.createElement('button');
+            bookButton.innerHTML='Book';
+            tripModel.appendChild(bookButton);
+
+            tripContainer.appendChild(tripModel);
+        }
+
     } catch (err) {
         console.log(err);
     }
